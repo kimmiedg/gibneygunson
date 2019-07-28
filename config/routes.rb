4779b6bb34733d1613fab19770a968bcf015e1be
iamkimmiedg <kanne.degracia@gmail.com>
2019-07-29 00:00:02 +0800

    Added contact us

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :services, only: [:index, :show]
   resources :subservices, only: [:index, :show]
   get 'documents_dashboard' => 'pages#documents_dashboard'
+  get 'contact_us' => 'pages#contact_us'
 end
